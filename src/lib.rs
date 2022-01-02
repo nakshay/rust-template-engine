0c0322c34e248d976445ee4252927d4e34136fa1
André M. Teixeira <andremarcondesteixeira@gmail.com>
2022-01-02 14:45:06 -0300

    gets rid of old string replace strategy

--- src/lib.rs
+++ src/lib.rs
@@ -1,191 +1,313 @@
-pub mod pair;
-pub mod render_strategy;
-pub mod template;
+pub fn render(template: String) -> String {
+    let mut tokenizer = Tokenizer::new(template.chars());
+    tokenizer.tokenize();
+    "".into()
+}
 
-use template::Context;
+struct Tokenizer<T: Iterator<Item = char>> {
+    buffer: Vec<char>,
+    curr_context: TokenizerContext,
+    prev_markers: (Option<char>, Option<char>),
+    template: T,
+    tokens: Vec<Token>,
+}
 
-pub fn render(
-    template: String,
-    context: Context,
-    strategy: fn(String, Context) -> String,
-) -> String {
-    strategy(template, context)
+#[derive(Debug, PartialEq, Clone)]
+enum Token {
+    Expression(String),
+    Statement(String),
+    Text(String),
+}
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum TokenizerContext {
+    ExpressionEnd,
+    ExpressionStart,
+    Start,
+    StatementEnd,
+    StatementStart,
+}
+
+impl<T: Iterator<Item = char>> Tokenizer<T> {
+    fn new(template: T) -> Self {
+        Tokenizer {
+            buffer: Vec::new(),
+            curr_context: TokenizerContext::Start,
+            prev_markers: (None, None),
+            template,
+            tokens: Vec::new(),
+        }
+    }
+
+    fn tokenize(&mut self) {
+        while let Some(char_value) = self.template.next() {
+            self.buffer.push(char_value);
+            let context = self.detect_context();
+            if context != self.curr_context {
+                self.curr_context = context;
+                self.make_token();
+            }
+        }
+
+        self.make_last_token();
+    }
+
+    fn detect_context(&self) -> TokenizerContext {
+        let last_two_chars = {
+            if self.buffer.len() > 1 {
+                let previous = self.buffer.get(self.buffer.len() - 2);
+                let current = self.buffer.last();
+                (previous, current)
+            } else if self.buffer.len() == 1 {
+                (None, self.buffer.last())
+            } else {
+                (None, None)
+            }
+        };
+
+        match last_two_chars {
+            (Some('{'), Some('{')) => TokenizerContext::ExpressionStart,
+            (Some('}'), Some('}')) => TokenizerContext::ExpressionEnd,
+            (Some('{'), Some('%')) => TokenizerContext::StatementStart,
+            (Some('%'), Some('}')) => TokenizerContext::StatementEnd,
+            _ => self.curr_context,
+        }
+    }
+
+    fn make_token(&mut self) {
+        let curr_2nd_marker = self.buffer.pop().unwrap();
+        let curr_1st_marker = self.buffer.pop().unwrap();
+        let token_value: String = self.buffer.iter().collect();
+
+        if token_value.trim().len() > 0 {
+            self.tokens.push(match self.curr_context {
+                TokenizerContext::ExpressionStart => Token::Text(token_value),
+                TokenizerContext::ExpressionEnd => Token::Expression(token_value.trim().into()),
+                TokenizerContext::StatementStart => Token::Text(token_value),
+                TokenizerContext::StatementEnd => Token::Statement(token_value.trim().into()),
+                _ => return,
+            });
+        } else if let (Some(prev_1st_marker), Some(prev_2nd_marker)) = self.prev_markers {
+            if curr_1st_marker != '{' {
+                self.tokens.push(Token::Text(format!(
+                    "{}{}{}{}{}",
+                    prev_1st_marker, prev_2nd_marker, token_value, curr_1st_marker, curr_2nd_marker
+                )));
+            }
+        }
+
+        self.prev_markers = (Some(curr_1st_marker), Some(curr_2nd_marker));
+        self.buffer = Vec::new();
+    }
+
+    fn make_last_token(&mut self) {
+        let mut rest = String::new();
+
+        if self.curr_context == TokenizerContext::ExpressionStart || self.curr_context == TokenizerContext::StatementStart {
+            rest = format!("{}{}{}", rest, self.prev_markers.0.unwrap(), self.prev_markers.1.unwrap());
+        }
+
+        if self.buffer.len() > 0 {
+            rest = format!("{}", self.buffer.iter().collect::<String>());
+        }
+
+        if rest.len() > 0 {
+            self.tokens.push(Token::Text(rest));
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use pair::{Pair, PairGenerator};
-    use render_strategy;
-    use std::fmt::{Display, Formatter, Result};
 
-    #[test]
-    fn can_pass_a_string_to_the_template_and_print_it() {
-        let context = Context::with(vec![Pair::of("name", "Rust")]);
-        let output = render(s("name: {{ name }}"), context, render_strategy::string_replace);
-        assert_eq!(output, s("name: Rust"));
+    #[derive(Debug, PartialEq)]
+    enum FormsOf<'a> {
+        Expressions(&'a [(&'static str, Token); 52]),
+        Texts(&'a [&'static str; 18]),
     }
 
     #[test]
-    fn can_pass_a_boolean_to_the_template_and_print_it() {
-        let context = Context::with(vec![Pair::of("is awesome", true)]);
-        let output = render(
-            s("is awesome: {{ is awesome }}"),
-            context,
-            render_strategy::string_replace,
-        );
-        assert_eq!(output, s("is awesome: true"));
-    }
+    fn test_many_combinations() {
+        let texts = [" ", "  ", "   ", "a", "ab", "abc", "{", "{{", "{%", "{{{", "{{%", "{%{", "}", "}}", "%}", "%}}", "}%}", "}}}"];
+        let expressions = [
+            ("{{}}", Token::Text("{{}}".into())),
+            ("{{ }}", Token::Text("{{ }}".into())),
+            ("{{  }}", Token::Text("{{  }}".into())),
+            ("{{   }}", Token::Text("{{   }}".into())),
+            ("{{a}}", Token::Expression("a".into())),
+            ("{{a }}", Token::Expression("a".into())),
+            ("{{a  }}", Token::Expression("a".into())),
+            ("{{a   }}", Token::Expression("a".into())),
+            ("{{ a}}", Token::Expression("a".into())),
+            ("{{ a }}", Token::Expression("a".into())),
+            ("{{ a  }}", Token::Expression("a".into())),
+            ("{{ a   }}", Token::Expression("a".into())),
+            ("{{  a}}", Token::Expression("a".into())),
+            ("{{  a }}", Token::Expression("a".into())),
+            ("{{  a  }}", Token::Expression("a".into())),
+            ("{{  a   }}", Token::Expression("a".into())),
+            ("{{   a}}", Token::Expression("a".into())),
+            ("{{   a }}", Token::Expression("a".into())),
+            ("{{   a  }}", Token::Expression("a".into())),
+            ("{{   a   }}", Token::Expression("a".into())),
+            ("{{aa}}", Token::Expression("aa".into())),
+            ("{{aa }}", Token::Expression("aa".into())),
+            ("{{aa  }}", Token::Expression("aa".into())),
+            ("{{aa   }}", Token::Expression("aa".into())),
+            ("{{ aa}}", Token::Expression("aa".into())),
+            ("{{ aa }}", Token::Expression("aa".into())),
+            ("{{ aa  }}", Token::Expression("aa".into())),
+            ("{{ aa   }}", Token::Expression("aa".into())),
+            ("{{  aa}}", Token::Expression("aa".into())),
+            ("{{  aa }}", Token::Expression("aa".into())),
+            ("{{  aa  }}", Token::Expression("aa".into())),
+            ("{{  aa   }}", Token::Expression("aa".into())),
+            ("{{   aa}}", Token::Expression("aa".into())),
+            ("{{   aa }}", Token::Expression("aa".into())),
+            ("{{   aa  }}", Token::Expression("aa".into())),
+            ("{{   aa   }}", Token::Expression("aa".into())),
+            ("{{aaa}}", Token::Expression("aaa".into())),
+            ("{{aaa }}", Token::Expression("aaa".into())),
+            ("{{aaa  }}", Token::Expression("aaa".into())),
+            ("{{aaa   }}", Token::Expression("aaa".into())),
+            ("{{ aaa}}", Token::Expression("aaa".into())),
+            ("{{ aaa }}", Token::Expression("aaa".into())),
+            ("{{ aaa  }}", Token::Expression("aaa".into())),
+            ("{{ aaa   }}", Token::Expression("aaa".into())),
+            ("{{  aaa}}", Token::Expression("aaa".into())),
+            ("{{  aaa }}", Token::Expression("aaa".into())),
+            ("{{  aaa  }}", Token::Expression("aaa".into())),
+            ("{{  aaa   }}", Token::Expression("aaa".into())),
+            ("{{   aaa}}", Token::Expression("aaa".into())),
+            ("{{   aaa }}", Token::Expression("aaa".into())),
+            ("{{   aaa  }}", Token::Expression("aaa".into())),
+            ("{{   aaa   }}", Token::Expression("aaa".into())),
+        ];
 
-    #[test]
-    fn can_pass_a_displayable_object_to_the_template_and_print_it() {
-        let context = Context::with(vec![Pair::of(
-            "displayable object",
-            get_displayable_object(),
-        )]);
-        let output = render(
-            s("displayable object: {{ displayable object }}"),
-            context,
-            render_strategy::string_replace,
-        );
-        assert_eq!(output, s("displayable object: bar"));
-    }
+        let combinations_to_test = [
+            [None, None, Some(FormsOf::Texts(&texts))],
+            [None, None, Some(FormsOf::Expressions(&expressions))],
+            [
+                None,
+                Some(FormsOf::Texts(&texts)),
+                Some(FormsOf::Expressions(&expressions)),
+            ],
+            [
+                None,
+                Some(FormsOf::Expressions(&expressions)),
+                Some(FormsOf::Texts(&texts)),
+            ],
+            [
+                None,
+                Some(FormsOf::Expressions(&expressions)),
+                Some(FormsOf::Expressions(&expressions)),
+            ],
+            [
+                Some(FormsOf::Texts(&texts)),
+                Some(FormsOf::Expressions(&expressions)),
+                Some(FormsOf::Texts(&texts)),
+            ],
+            [
+                Some(FormsOf::Texts(&texts)),
+                Some(FormsOf::Expressions(&expressions)),
+                Some(FormsOf::Expressions(&expressions)),
+            ],
+            [
+                Some(FormsOf::Expressions(&expressions)),
+                Some(FormsOf::Texts(&texts)),
+                Some(FormsOf::Expressions(&expressions)),
+            ],
+            [
+                Some(FormsOf::Expressions(&expressions)),
+                Some(FormsOf::Expressions(&expressions)),
+                Some(FormsOf::Texts(&texts)),
+            ],
+            [
+                Some(FormsOf::Expressions(&expressions)),
+                Some(FormsOf::Expressions(&expressions)),
+                Some(FormsOf::Expressions(&expressions)),
+            ],
+        ];
 
-    #[test]
-    fn can_pass_a_double_to_the_template_and_print_it() {
-        let context = Context::with(vec![Pair::of("a double", 1.2)]);
-        let output = render(
-            s("a double: {{ a double }}"),
-            context,
-            render_strategy::string_replace,
-        );
-        assert_eq!(output, s("a double: 1.2"));
-    }
-
-    #[test]
-    fn can_pass_a_hashmap_to_the_template_and_print_it() {
-        let context = Context::with(vec![Pair::of(
-            "a map",
-            Context::with(vec![Pair::of(
-                "nested",
-                Context::with(vec![Pair::of("nested nested", "foo")]),
-            )]),
-        )]);
-        let output = render(
-            s("a map: {{ a map.nested.nested nested }}"),
-            context,
-            render_strategy::string_replace,
-        );
-        assert_eq!(output, s("a map: foo"));
-    }
-
-    #[test]
-    fn can_pass_an_integer_to_the_template_and_print_it() {
-        let context = Context::with(vec![Pair::of("an integer", 100)]);
-        let output = render(
-            s("an integer: {{ an integer }}"),
-            context,
-            render_strategy::string_replace,
-        );
-        assert_eq!(output, s("an integer: 100"));
-    }
-
-    #[test]
-    fn can_print_multiple_variables_inside_the_same_template() {
-        let context = Context::with(vec![
-            Pair::of(
-                "context",
-                Context::with(vec![
-                    Pair::of(
-                        "context",
-                        Context::with(vec![
-                            Pair::of("displayable", get_displayable_object()),
-                            Pair::of("bool", true),
-                            Pair::of("f64", 1.23),
-                            Pair::of("isize", 123456),
-                            Pair::of("string", "foo"),
-                            Pair::of("string reference", "bar"),
-                        ]),
-                    ),
-                    Pair::of("displayable", get_displayable_object()),
-                    Pair::of("bool", true),
-                    Pair::of("f64", 1.23),
-                    Pair::of("isize", 123456),
-                    Pair::of("string", "foo"),
-                    Pair::of("string reference", "bar"),
-                ]),
-            ),
-            Pair::of("displayable", get_displayable_object()),
-            Pair::of("bool", true),
-            Pair::of("f64", 1.23),
-            Pair::of("isize", 123456),
-            Pair::of("string", "foo"),
-            Pair::of("string reference", "bar"),
-        ]);
-        let template = s("
-            displayable: {{ displayable }}
-            bool: {{ bool }}
-            f64: {{ f64 }}
-            isize: {{ isize }}
-            string: {{ string }}
-            string reference: {{ string reference }}
-
-            context.displayable: {{ context.displayable }}
-            context.bool: {{ context.bool }}
-            context.f64: {{ context.f64 }}
-            context.isize: {{ context.isize }}
-            context.string: {{ context.string }}
-            context.string reference: {{ context.string reference }}
-
-            context.context.displayable: {{ context.context.displayable }}
-            context.context.bool: {{ context.context.bool }}
-            context.context.f64: {{ context.context.f64 }}
-            context.context.isize: {{ context.context.isize }}
-            context.context.string: {{ context.context.string }}
-            context.context.string reference: {{ context.context.string reference }}
-        ");
-        let output = render(template, context, render_strategy::string_replace);
-        let expected_output = s("
-            displayable: bar
-            bool: true
-            f64: 1.23
-            isize: 123456
-            string: foo
-            string reference: bar
-
-            context.displayable: bar
-            context.bool: true
-            context.f64: 1.23
-            context.isize: 123456
-            context.string: foo
-            context.string reference: bar
-
-            context.context.displayable: bar
-            context.context.bool: true
-            context.context.f64: 1.23
-            context.context.isize: 123456
-            context.context.string: foo
-            context.context.string reference: bar
-        ");
-        assert_eq!(output, expected_output);
-    }
-
-    struct CanBeDisplayed {
-        foo: String,
-    }
-
-    impl Display for CanBeDisplayed {
-        fn fmt(&self, f: &mut Formatter) -> Result {
-            write!(f, "{}", self.foo)
+        for combination in combinations_to_test {
+            make_combinations(&combination, 2, "".into(), [None, None, None]);
         }
     }
 
-    fn get_displayable_object() -> Box<dyn Display> {
-        Box::new(CanBeDisplayed { foo: s("bar") })
+    fn make_combinations(
+        scenario: &[Option<FormsOf>; 3],
+        position: usize,
+        template: String,
+        tokens: [Option<Token>; 3],
+    ) {
+        match &scenario[position] {
+            Some(forms) => match forms {
+                FormsOf::Texts(texts) => {
+                    if position == 0 {
+                        for text in *texts {
+                            let mut tokens = tokens.clone();
+                            tokens[position] = Some(Token::Text(String::from(*text)));
+                            assert_template_has_tokens(format!("{}{}", *text, template), tokens);
+                        }
+                    } else {
+                        for text in *texts {
+                            let mut tokens = tokens.clone();
+                            tokens[position] = Some(Token::Text(String::from(*text)));
+                            make_combinations(
+                                scenario,
+                                position - 1,
+                                format!("{}{}", *text, template),
+                                tokens,
+                            );
+                        }
+                    }
+                }
+                FormsOf::Expressions(expressions) => {
+                    if position == 0 {
+                        for expression in (*expressions).clone() {
+                            let mut tokens = tokens.clone();
+                            tokens[position] = Some(expression.1);
+                            assert_template_has_tokens(
+                                format!("{}{}", expression.0, template),
+                                tokens,
+                            );
+                        }
+                    } else {
+                        for expression in (*expressions).clone() {
+                            let mut tokens = tokens.clone();
+                            tokens[position] = Some(expression.1);
+                            make_combinations(
+                                scenario,
+                                position - 1,
+                                format!("{}{}", expression.0, template),
+                                tokens,
+                            );
+                        }
+                    }
+                }
+            },
+            None => {
+                let tokens = tokens.clone();
+                assert_template_has_tokens(template, tokens);
+            }
+        }
     }
 
-    /// Creates a Sring instance
-    fn s(input: &str) -> String {
-        String::from(input)
+    fn assert_template_has_tokens(template: String, expected_tokens: [Option<Token>; 3]) {
+        let expected_tokens: Vec<Token> = expected_tokens
+            .iter()
+            .filter(|x| x.is_some())
+            .map(|x| x.clone().unwrap())
+            .collect();
+        let mut tokenizer = Tokenizer::new(template.chars());
+        tokenizer.tokenize();
+        let actual_tokens = tokenizer.tokens;
+        let message = format!(
+            "expected template \"{}\" to have tokens {:?}, but received {:?}",
+            template, expected_tokens, actual_tokens
+        );
+        assert_eq!(actual_tokens, expected_tokens, "{}", message);
+        println!("template \"{}\" should have tokens {:?} ... ok", template, expected_tokens);
     }
 }
--- src/render_strategy/mod.rs
+++
@@ -1,5 +0,0 @@
-mod interpreter;
-mod string_replace;
-
-pub use interpreter::interpreter;
-pub use string_replace::string_replace;
