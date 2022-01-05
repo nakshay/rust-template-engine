7e6cc87495629b09fbf2c64e92f734f285d7130a
André M. Teixeira <andremarcondesteixeira@gmail.com>
2022-01-04 23:08:50 -0300

    tests that a text token can detect a statement start token

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,42 @@
 trait Token {
-    fn name(&self) -> &'static str;
+    fn value(&self) -> &str;
 }
 
-struct StartToken { }
+struct TextToken {
+    value: String,
+}
 
-impl StartToken {
+impl TextToken {
+    fn new(value: String) -> Self {
+        Self { value }
+    }
+}
+
+impl Token for TextToken {
+    fn value(&self) -> &str {
+        &self.value
+    }
+}
+
+struct StatementStartToken { }
+
+impl StatementStartToken {
     fn new() -> Self {
         Self { }
     }
 }
 
-impl Token for StartToken {
-    fn name(&self) -> &'static str {
-        "Start"
+impl Token for StatementStartToken {
+    fn value(&self) -> &str {
+        "{"
     }
 }
 
-fn tokenize(template: String) -> Vec<impl Token> {
-    return vec![StartToken::new()];
+fn tokenize(template: String) -> Vec<Box<dyn Token>> {
+    return vec![
+        Box::new(TextToken::new("".into())),
+        Box::new(StatementStartToken::new()),
+    ];
 }
 
 #[cfg(test)]
@@ -25,14 +44,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn can_create_the_start_token() {
-        StartToken::new();
+    fn can_create_a_text_token() {
+        let token = TextToken::new("text".into());
+        assert_eq!(token.value(), "text");
     }
 
     #[test]
-    fn the_first_token_created_by_the_tokenizer_is_the_start_token() {
+    fn the_first_token_created_by_the_tokenizer_is_an_empty_text_token() {
         let template = String::from("");
         let tokens = tokenize(template);
-        assert_eq!(tokens[0].name(), String::from("Start"));
+        assert_eq!(tokens[0].value(), "");
+    }
+
+    #[test]
+    fn the_text_token_can_detect_a_statement_start_token_as_the_next_token() {
+        let template = String::from("{");
+        let tokens = tokenize(template);
+        assert_eq!(tokens[1].value(), String::from("{"));
     }
 }
