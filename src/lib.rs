4a3c5b461b44ed91b89109a5a5bb707b22215a76
André M. Teixeira <andremarcondesteixeira@gmail.com>
2022-01-04 22:28:13 -0300

    tests that the first token created by the tokenizer is a Start token

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,23 @@
+trait Token {
+    fn name(&self) -> &'static str;
+}
+
 struct StartToken { }
 
 impl StartToken {
     fn new() -> Self {
         Self { }
     }
+}
+
+impl Token for StartToken {
+    fn name(&self) -> &'static str {
+        "Start"
+    }
+}
+
+fn tokenize(template: String) -> Vec<impl Token> {
+    return vec![StartToken::new()];
 }
 
 #[cfg(test)]
@@ -13,5 +27,12 @@ mod tests {
     #[test]
     fn can_create_the_start_token() {
         StartToken::new();
+    }
+
+    #[test]
+    fn the_first_token_created_by_the_tokenizer_is_the_start_token() {
+        let template = String::from("");
+        let tokens = tokenize(template);
+        assert_eq!(tokens[0].name(), String::from("Start"));
     }
 }
